8b11f4f5db7b951b77cbc3e3f5e817bb104eeff6
Will Mangimelli <willwinman@gmail.com>
2019-06-12 16:48:50 -0500

    debuggin

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,6 +10,7 @@ router.get("/", function(req, res) {
 
 router.get("/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
+  console.log("get /burgers");
   burger.all(function(burgerData) {
     // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
     res.render("index", { burger_data: burgerData });
